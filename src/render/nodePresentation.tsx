84939779b9d322e25ba57662428098a81fa51dc0
Tom Bigelajzen <public@tombigel.com>
2026-06-13 03:36:37 +0300

    fix(security): remove stale code escaping helper

--- src/render/nodePresentation.tsx
+++ src/render/nodePresentation.tsx
@@ -522,11 +522,6 @@ function getPageCurrentProps(
     : {};
 }
 
-
-function escapeHtml(s: string): string {
-  return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-}
-
 export function renderLeafContent(
   node: LeafNode,
   options: RenderLeafContentOptions & {
