2cca6a56b221b17de6de28a533121964287a6c57
Tom Bigelajzen <public@tombigel.com>
2026-06-14 18:36:58 +0300

    fix(animation): declare motion dependency

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,4 +1,4 @@
-export const PROJECT_VERSION = '0.9.2';
-export const DOCUMENT_MODEL_VERSION = '1.5.144';
-export const API_VERSION = '2.2.2';
-export const EDITOR_VERSION = '0.16.2';
+export const PROJECT_VERSION = '0.9.3';
+export const DOCUMENT_MODEL_VERSION = '1.5.145';
+export const API_VERSION = '2.2.3';
+export const EDITOR_VERSION = '0.16.3';
